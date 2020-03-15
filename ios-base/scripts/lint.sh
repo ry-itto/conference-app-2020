1368b2062c825eadafcbe74ae1de540adf170986
ryoya ito <horusu.dqx@gmail.com>
2020-03-15 16:51:48 +0900

    test swiftlint

--- ios-base/scripts/lint.sh
+++ ios-base/scripts/lint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 git status --porcelain | grep -e '^[ ?][^D] ".*\.swift"$' | awk -F'"' '{print $2}' | while read filename; do
-  mint run realm/swiftlint swiftlint --path "$SRCROOT/../$filename"
+  #mint run realm/swiftlint swiftlint --path "$SRCROOT/../$filename"
+  $SRCROOT/../../SwiftLint/.build/release/swiftlint --path "$SRCROOT/../$filename"
 done
